4fb084b70c859c58aa56b968c036e5eafd02e483
A-Lyaska <2710747@mail.ru>
2022-05-12 11:51:38 +0300

    Start

---
+++ cmake-build-debug/Chessboard.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 27107 on 11.05.2022.
+//
+
+#include "Chessboard.h"
---
+++ cmake-build-debug/Chessboard.h
@@ -0,0 +1,24 @@
+//
+// Created by 27107 on 11.05.2022.
+//
+
+#ifndef CHESS_CHESSBOARD_H
+#define CHESS_CHESSBOARD_H
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Chessboard
+{
+public:
+    int lx, ly;
+    ChessBoard() {
+        lx = 8;
+        ly = 8;
+    }
+
+};
+
+
+#endif //CHESS_CHESSBOARD_H
